4e4d96fe6bba324e3638e1ed5ef4fc5f9a7bf2a0
Andrew Primiano <agp44843@uga.edu>
2026-01-23 19:42:43 -0500

    using original code for hardware debouncing

--- deliverable3.py
+++ deliverable3.py
@@ -13,24 +13,21 @@ pi.set_mode(ROTARY_PIN, pigpio.INPUT)
 # Most encoders pull to GND when pressed, so we use PUD_UP
 pi.set_pull_up_down(ROTARY_PIN, pigpio.PUD_UP)
 
-# Set glitch filter: ignore pulses shorter than 50ms (50000 us)
-pi.set_glitch_filter(ROTARY_PIN, 50000)
+# Glitch filter acts as a hardware-level debounce (3000 microseconds)
+pi.set_glitch_filter(ROTARY_PIN, 3000)
 
-button_state = 0
+led_state = False
 
-def clean_callback(gpio, level, tick):
-    global button_state
+def toggle_callback(gpio, level, tick):
+    global led_state
+    # level 0 means the button was pressed (pulled to ground)
+    if level == 0:
+        led_state = not led_state
+        pi.write(LED_PIN, led_state)
+        print(f"LED is now {'ON' if led_state else 'OFF'}")
 
-    if level == 1:
-        print("Button Pressed!")
-        button_state = 1
-        pi.write(LED_PIN, 1)
-    elif level == 0:
-        print("Button Released!")
-        button_state = 0
-        pi.write(LED_PIN, 0)
-
-cb = pi.callback(ROTARY_PIN, pigpio.EITHER_EDGE, clean_callback)
+# Set up the interrupt/callback
+cb = pi.callback(ROTARY_PIN, pigpio.FALLING_EDGE, toggle_callback)
 
 try:
     print("System Ready. Press the encoder button to toggle...")
