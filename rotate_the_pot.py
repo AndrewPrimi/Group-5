d0f5a7b65d6833f704eda64b669e59afd7b8e3c9
BanzaiBMO <bmogamerkun@gmail.com>
2026-02-08 10:50:39 -0500

    fallin_edge

--- rotate_the_pot.py
+++ rotate_the_pot.py
@@ -228,7 +228,7 @@ try:
 
         cb_enc_a = pi.callback(PIN_A, pigpio.EITHER_EDGE,
                                menu_encoder_callback)
-        cb_enc_b = pi.callback(PIN_B, pigpio.EITHER_EDGE,
+        cb_enc_b = pi.callback(PIN_B, pigpio.FALLING_EDGE,
                                menu_encoder_callback)
         cb_btn = pi.callback(
             rotaryEncoder_pin, pigpio.FALLING_EDGE, menu_button_callback)
@@ -246,10 +246,9 @@ try:
         set_lcd()
 
         cb_enc_1 = pi.callback(PIN_A, pigpio.EITHER_EDGE, encoder_callback)
-        cb_enc_b = pi.callback(PIN_B, pigpio.EITHER_EDGE, encoder_callback)
+        cb_enc_b = pi.callback(PIN_B, pigpio.FALLING_EDGE, encoder_callback)
         cb_btn = pi.callback(
-            # previously was EITHER_EDGE before.
-            rotaryEncoder_pin, pigpio.FALLING_EDGE, callback_set_digi)
+            rotaryEncoder_pin, pigpio.EITHER_EDGE, callback_set_digi)
         active_callbacks = [cb_enc_a, cb_enc_b, cb_btn]
 
         while not isMainPage:
