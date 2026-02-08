8ca5547f5eb632a64deba5e8e16144ec8ef0db25
BanzaiBMO <bmogamerkun@gmail.com>
2026-02-08 18:17:11 -0500

    step value adjust

--- rotate_the_pot.py
+++ rotate_the_pot.py
@@ -54,7 +54,14 @@ def set_digipot_step(step_value):
     """Write data bytes to the currently selected MCP4131's SPI device handle."""
     if 0 <= step_value <= MAX_STEPS:
         h = handle_pot1 if selected_pot == 0 else handle_pot2
-        pi.spi_write(h, [0x00, step_value - 100])
+
+        # adjust step_value to add/subtract
+        if step_value < 12:
+            step_value -= 100
+        else:
+            step_value += 100
+
+        pi.spi_write(h, [0x00, step_value])
         approx_ohms = step_to_ohms(step_value)
         print(
             f"Pot {selected_pot + 1} | Step: {step_value:3d} | Approx: {approx_ohms:7.1f} Ohms")
