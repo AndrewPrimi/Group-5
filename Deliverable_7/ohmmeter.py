ed96416505aae29b5f706ce432cea2fbbca03fa5
BanzaiBMO <bmogamerkun@gmail.com>
2026-03-15 18:14:23 -0400

    comp variable recognized

--- Deliverable_7/ohmmeter.py
+++ Deliverable_7/ohmmeter.py
@@ -101,7 +101,7 @@ def sar_measure(pi, spi_handle, comp_pin):
         comp = pi.read(comp_pin)
         #if comp == 1:                  # V_midpoint > V_wiper: keep this bit
         #    step = trial
-        if pi.read(comp_pin) == 0: # invert logic kai devito
+        if comp == 0: # invert logic kai devito
             step = trial
 
     # Final write to leave DAC at the converged value
