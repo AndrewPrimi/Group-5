be09c3a976b087b1fb55dd4cf1450658876b8e6d
BanzaiBMO <bmogamerkun@gmail.com>
2026-03-17 17:35:29 -0400

    moved square_wave.py to Deliverable8

---
+++ Deliverable8/square_wave.py
@@ -0,0 +1,17 @@
+import pigpio
+import math
+import time
+
+pi = pigpio.pi()
+
+GPIO = 13
+FREQ = 1000         # 1 kHz
+DUTY = 500000       # 50% duty cycle 
+
+pi.set_mode(GPIO, pigpio.OUTPUT)
+pi.hardware_PWM(GPIO, FREQ, DUTY)
+
+time.sleep(10)
+
+pi.hardware_PWM(GPIO, 0, 0)  # stop
+pi.stop()
