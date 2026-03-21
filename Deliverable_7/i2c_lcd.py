92464befe5f9bf6b3a8f859996ce40da26f6c623
Kai DeVito <krd55610@uga.edu>
2026-03-21 14:02:16 -0400

    Update i2c_lcd.py

--- Deliverable_7/i2c_lcd.py
+++ Deliverable_7/i2c_lcd.py
@@ -322,9 +322,9 @@ if __name__ == "__main__":
         # Leave addr=None so it auto-tries common LCD addresses.
         # If you want to force one, use addr=0x27 or addr=0x24, etc.
         #lcd_display = lcd(pi, addr=None, width=20)
-        lcd_display = lcd(pi, addr=None, width=20, RS=0,
+        lcd_display = i2c_lcd(pi, addr=None, width=20, RS=0,
                           #RW=1,
-                          E=2, BL=3, B4=4)
+                          E=2, BL=3, B4=4, debug=True)
 
         lcd_display.backlight(True)
         lcd_display.clear()
