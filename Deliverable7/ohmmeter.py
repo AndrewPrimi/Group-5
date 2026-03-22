948da3fe0ecde10f098c6aed1747cce6444b27be
Logan-Griffy <157719750+Logan-Griffy@users.noreply.github.com>
2026-03-21 21:10:31 -0400

    Update ohmmeter.py

--- Deliverable7/ohmmeter.py
+++ Deliverable7/ohmmeter.py
@@ -1,5 +1,6 @@
 """
 ohmmeter.py
+Wrapper helpers for ohmmeter operation.
 """
 
 from sar_logic import SAR_ADC
@@ -11,4 +12,4 @@ R_KNOWN = 1000.0
 def read_ohmmeter_value(sar: SAR_ADC,
                         vref: float = OHMMETER_VREF,
                         r_known: float = R_KNOWN):
-    return sar.read_ohms(vref, r_known)
+    return sar.read_ohms(vref, r_known))
