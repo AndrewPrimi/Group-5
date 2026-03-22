f6231588c8bf3d3e69acf372203d12f9baf0780c
Logan-Griffy <157719750+Logan-Griffy@users.noreply.github.com>
2026-03-21 21:04:10 -0400

    Update main.py

--- Deliverable7/main.py
+++ Deliverable7/main.py
@@ -68,7 +68,7 @@ def main():
 
             elif choice == "3":
                 voltage, v_code = read_voltmeter_value(voltage_sar)
-                ohms, o_code = read_ohmmeter_value(ohms_sar, vref=3.3, r_known=1000.0)
+                ohms, code = read_ohmmeter_value(ohms_sar, vref=3.3, r_known=2000.0)
 
                 print(f"Voltage    = {voltage:.3f} V | code = {v_code}")
                 print(f"Resistance = {ohms:.3f} ohms | code = {o_code}")
