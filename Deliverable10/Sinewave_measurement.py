f179f4b0e8309de7d12a9c27056af5b9ffeeaeb3
Kai DeVito <krd55610@uga.edu>
2026-04-06 20:20:20 -0400

    fix 2

--- Deliverable10/Sinewave_measurement.py
+++ Deliverable10/Sinewave_measurement.py
@@ -1,7 +1,10 @@
 import pigpio
+import time
+from collections import deque
 
 GPIO_PIN = 5  # Comparator output
 MIN_DT_US = 50
+MEDIAN_LEN = 5
 
 class FrequencyMeter:
     def __init__(self, pi, debug=False):
@@ -11,13 +14,15 @@ class FrequencyMeter:
         self.last_tick = None
         self.frequency = 0.0
 
+        self.dt_buffer = deque(maxlen=5) # this
+        
         pi.set_mode(GPIO_PIN, pigpio.INPUT)
 
         # Register callback on rising edge
         self.cb = pi.callback(GPIO_PIN, pigpio.RISING_EDGE, self._cb)
 
     def _cb(self, gpio, level, tick):
-        if self.last_tick is not None:
+        """if self.last_tick is not None:
             dt = pigpio.tickDiff(self.last_tick, tick)  # microseconds
 
             if dt < MIN_DT_US:
@@ -25,6 +30,23 @@ class FrequencyMeter:
 
             self.frequency = 1_000_000 / dt
 
+        self.last_tick = tick"""
+
+        if self.last_tick is not None:
+            dt = pigpio.tickDiff(self.last_tick, tick)  # microseconds
+
+            if dt < MIN_DT_US:
+                return
+
+            self.dt_buffer.append(dt)
+
+            if len(self.dt_buffer) == MEDIAN_LEN:
+                dt_median = sorted(self.dt_buffer)[MEDIAN_LEN // 2]
+                self.frequency = 1_000_000 / dt_median
+
+                if self.debug:
+                    print(f"DTs: {list(self.dt_buffer)}, median dt: {dt_median}, freq: {self.frequency:.2f} Hz")
+                    
         self.last_tick = tick
 
     def get_frequency(self):
